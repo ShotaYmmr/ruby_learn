9c05fc5cd21df11fd5db7111c8ba555f60f5909e
ShotaYmmr <sho.yama.aws@gmail.com>
2020-09-25 18:20:45 +0900

    文字列の学習

--- string_test.rb
+++ string_test.rb
@@ -20,6 +20,9 @@ puts %q?He said, "Don't speak."?
 puts %q{He said, "Don't speak."}
 
 # ヒアドキュメントの使い方
+puts "line1,
+line2"
+
 a = <<TEXT
 これはヒアドキュメントです。
 複数行に渡る長い文字列に使用すると便利らしい。
@@ -35,3 +38,76 @@ HTML
 
 puts b
 
+def some_method
+    <<-TEXT
+これはヒアドキュメントです。
+<<-を使うと最後の識別子をインデントさせることができます。
+    TEXT
+end
+
+puts some_method
+
+def some_method2
+    <<~TEXT
+        これはヒアドキュメントです。
+        <<~を使うと内部文字列のインデント部分が無視されます。
+    TEXT
+end
+
+puts some_method2
+
+name = 'Alice'
+
+a = <<TEXT
+ようこそ #{name}さん
+以下のメッセージを御覧ください。
+※ヒアドキュメント内で式展開が可能。
+TEXT
+puts a
+
+b = <<'TEXT'
+ようこそ #{name}さん
+以下のメッセージを御覧ください。
+※シングルクォートをつけるとヒアドキュメント内での式展開が無効化。
+TEXT
+puts b
+
+c = <<"TEXT"
+ようこそ #{name}さん
+以下のメッセージを御覧ください。
+※ダブルクォートをつけるとヒアドキュメント内での式展開が有効化。
+TEXT
+puts c
+
+# ヒアドキュメントを直接引数として渡す
+# (prependは渡された文字列の末尾に追加するメソッド)
+a = 'Ruby'
+a.prepend(<<TEXT)
+Java
+PHP
+TEXT
+
+puts a
+
+# ヒアドキュメントで作成した文字列に対して、直接upcaseメソッドを呼び出す
+# (upcaseは文字列をすべて大文字にするメソッド)
+b = <<TEXT.upcase
+Hello,
+Good-bye.
+TEXT
+puts b
+
+puts sprintf('%0.3f',1.2)
+puts sprintf('%0.3f + %0.3f',1.2 , 0.48)
+
+# 数値を文字列に
+123.to_s
+
+# 配列を連結する
+[10,20,300].join
+
+# 演算子を使って繰り返し
+'Hi!' * 10
+
+# String.newを使って新しい文字列を作成する（めったに使わないらしい）
+String.new('Hello world')
